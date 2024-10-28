1dad637ec4f14c1367c66b5cd47d50988854a170
nicoperez12 <nicolas.perezp@live.cl>
2024-10-28 00:09:51 -0300

    avance compra con wallet

--- src/components/SelectPaymentMethod.jsx
+++ src/components/SelectPaymentMethod.jsx
@@ -1,8 +1,8 @@
-
 import React, { useState } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 import axios from 'axios';
 import { PopupCancelledError } from '@auth0/auth0-react';
+const API_URL = import.meta.env.VITE_API_URL;
 
 const SelectPaymentMethod = () => {
   const navigate = useNavigate();
@@ -12,10 +12,31 @@ const SelectPaymentMethod = () => {
 
   console.log("la quantity es", quantity) 
   // Función para manejar la compra con Wallet
-  const handleWalletPurchase = (e) => {
+  const handleWalletPurchase = async (e) => {
     e.preventDefault();
     // Lógica para procesar el pago con Wallet o redirigir
-    navigate('/wallet-purchase');
+    try {
+      // const tokenParts = user.sub.split('|');
+      // const token = tokenParts.length > 1 ? tokenParts[1] : user.sub;
+      const response = await axios.post(`${API_URL}/requests`,
+        {
+          group_id: group_id, 
+          fixture_id: fixture_id,
+          league_name: league_name,
+          round: round,
+          date: date,
+          quantity: quantity, 
+          result: result,
+          user_token: user_token,
+          deposit_token: user_token
+        }
+      );
+      console.log(response);
+    } catch (error) {
+      console.error('Error fetching fixture detail:', error);
+    }
+    
+    navigate(`/fixture/${fixture_id}`);
   };
 
   // Función para manejar la compra con Webpay
@@ -25,7 +46,7 @@ const SelectPaymentMethod = () => {
 
     try {
       // Primera llamada para crear la solicitud y obtener el request_id
-      const requestResponse = await axios.post('http://localhost:3000/requests', {
+      const requestResponse = await axios.post(`${API_URL}/requests`, {
         group_id,
         fixture_id,
         league_name,
@@ -50,7 +71,7 @@ const SelectPaymentMethod = () => {
       // asumiendo que hay un endpoint para eso.
       const request_id = requestResponse.data.request_id; // Asumiendo que la respuesta contiene request_id
       console.log("el request id es", request_id)
-      const transactionResponse = await axios.post('http://localhost:3000/transactions/create', {
+      const transactionResponse = await axios.post(`${API_URL}/transactions/create`, {
         request_id: request_id,
         quantity: quantity
       });
@@ -58,7 +79,7 @@ const SelectPaymentMethod = () => {
       // Suponiendo que la respuesta contiene los datos necesarios para la confirmación
       const { token, url, title, type, amount: transactionAmount } = transactionResponse.data;
 
-      const updateResponseRequest = await axios.patch(`http://localhost:3000/requests/${request_id}`, {
+      const updateResponseRequest = await axios.patch(`${API_URL}/requests/${request_id}`, {
         deposit_token: token, // Asume que `token` es el nuevo deposit_token
       });
 
